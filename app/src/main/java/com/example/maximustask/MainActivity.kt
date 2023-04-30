0e8ea2b186b9e36a8cc8b0b4f1d07ab5df7d5178
SaadullahTariq <saadullahtariqoffical@gmail.com>
2023-04-30 18:00:52 +0500

    Native AD is Implemented...

--- app/src/main/java/com/example/maximustask/MainActivity.kt
+++ app/src/main/java/com/example/maximustask/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.maximustask
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.maximustask.api.FactService
@@ -10,6 +13,12 @@ import com.example.maximustask.models.CatFacts
 import com.example.maximustask.repository.FactsRepository
 import com.example.maximustask.viewmodels.MainViewModel
 import com.example.maximustask.viewmodels.MainViewModelFactory
+import com.google.android.ads.nativetemplates.NativeTemplateStyle
+import com.google.android.ads.nativetemplates.TemplateView
+import com.google.android.gms.ads.*
+import com.google.android.gms.ads.nativead.NativeAd
+import com.google.android.gms.ads.nativead.NativeAdOptions
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,6 +29,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // ADs Initialization
+        MobileAds.initialize(this) {}
+
+        val adLoader = AdLoader.Builder(this, resources.getString(R.string.AdmobNative))
+            .forNativeAd { ad : NativeAd ->
+                // Show the ad.
+                val styles =
+                    NativeTemplateStyle.Builder().withMainBackgroundColor(ColorDrawable(Color.WHITE)).build()
+                val template = findViewById<TemplateView>(R.id.my_template)
+                template.setStyles(styles)
+                template.setNativeAd(ad)
+
+                // If this callback occurs after the activity is destroyed, you
+                // must call destroy and return or you may get a memory leak.
+                // Note `isDestroyed` is a method on Activity.
+                if (isDestroyed) {
+                    ad.destroy()
+                    return@forNativeAd
+                }
+            }
+            .withAdListener(object : AdListener() {
+                override fun onAdFailedToLoad(adError: LoadAdError) {
+                    // Handle the failure by logging, altering the UI, and so on.
+                    Toast.makeText(this@MainActivity, "Failed to Load Native Ad", Toast.LENGTH_SHORT).show()
+                }
+            })
+            .withNativeAdOptions(
+                NativeAdOptions.Builder()
+                // Methods in the NativeAdOptions.Builder class can be
+                // used here to specify individual options settings.
+                .build())
+            .build()
+
+        // Making AD request to Google's service
+        adLoader.loadAd(AdRequest.Builder().build())
 
         val factService = RetrofitHelper.getInstance().create(FactService::class.java)
 
